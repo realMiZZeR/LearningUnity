544bc6e9e69c4ce7ff870b064cb6ead0f8dbb0fd
MiZZeR <87935999+realMiZZeR@users.noreply.github.com>
2022-09-09 16:47:38 +0700

    let's start
    first in

---
+++ Assets/Scripts/UIWidgetButton/UIWidgetButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIWidgetButton : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private UIWidgetButtonAnimator _animator;
+    [SerializeField] private float _lifeTime;
+
+    private void OnEnable()
+    {
+        this._animator.OnAppearAnimationOverEvent += OnAppearAnimationOver;
+    }
+
+    private void OnDisable()
+    {
+        this._animator.OnAppearAnimationOverEvent -= OnAppearAnimationOver;
+    }
+
+    private void OnAppearAnimationOver()
+    {
+        StartCoroutine(LifeRoutine());
+    }
+
+    private IEnumerator LifeRoutine()
+    {
+        yield return new WaitForSeconds(this._lifeTime);
+        this._animator.PlayHide();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        this._animator.OnClickAnimationEvent += OnClickAnimation;
+
+        _animator.Handle_OnClickAnimation();
+    }
+
+    private void OnClickAnimation()
+    {
+        StopCoroutine(LifeRoutine());
+        this._animator.SetColor();
+    }
+}
---
+++ Assets/Scripts/UIWidgetButton/UIWidgetButtonAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class UIWidgetButtonAnimator : MonoBehaviour
+{
+    public event Action OnAppearAnimationOverEvent;
+    public event Action OnClickAnimationEvent;
+
+    [SerializeField] private Animator _animator;
+
+    public void PlayHide()
+    {
+        this._animator.SetTrigger("hide");
+    }
+
+    public void SetColor()
+    {
+        _animator.SetTrigger("color");
+    }
+
+    private void Handle_AppearAnimationOver()
+    {
+        this.OnAppearAnimationOverEvent?.Invoke();
+    }
+
+    public void Handle_OnClickAnimation()
+    {
+        OnClickAnimationEvent?.Invoke();
+    }
+}
