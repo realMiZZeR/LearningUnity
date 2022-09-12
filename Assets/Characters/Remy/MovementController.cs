909973ceb09e3b1f0d2915497241ebf178cfb66c
MiZZeR <87935999+realMiZZeR@users.noreply.github.com>
2022-09-12 16:47:09 +0700

    some character animations

---
+++ Assets/Characters/Remy/MovementController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    public float speed = 3f;
+
+    private Animator _animator;
+    private float _verticalMovement;
+    private float _horizontalMovement;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _verticalMovement = Input.GetAxis("Vertical");
+        _horizontalMovement = Input.GetAxis("Horizontal");
+
+        _animator.SetFloat("movementSpeed", _verticalMovement);
+    }
+}
