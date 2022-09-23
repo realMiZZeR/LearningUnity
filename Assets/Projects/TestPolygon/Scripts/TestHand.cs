57e8b682b824491cd19d362bd03037a6b843db16
MiZZeR <diamondAlma32@gmail.com>
2022-09-23 15:55:18 +0700

    garbage :)

---
+++ Assets/Projects/TestPolygon/Scripts/TestHand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHand : MonoBehaviour
+{
+    public LayerMask wallLayer;
+
+    private void FixedUpdate()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, .1f, wallLayer))
+        {
+            Debug.Log("p: " + hit.point);
+            Debug.Log("n: "+ hit.point.normalized);
+            Debug.Log("m: " + hit.point.magnitude);
+            Debug.DrawRay(transform.position, transform.forward * .1f, Color.red);
+            transform.position = hit.point;
+            transform.rotation = Quaternion.LookRotation(hit.point.normalized);
+        }
+    }
+}
