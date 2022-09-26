4767ac33c05d62a9aadc187e149d391b71cdab36
MiZZeR <diamondAlma32@gmail.com>
2022-09-26 16:52:49 +0700

    Fire

---
+++ Assets/Fire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+    private void OnEnable()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleTrigger()
+    {
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        for(int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            Debug.Log(p);
+            enter[i] = p;
+        }
+
+        Debug.Log(numEnter);
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+    }
+}
